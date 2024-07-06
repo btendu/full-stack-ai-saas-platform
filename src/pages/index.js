3eb9d65a0c9fe79cb0d8b1ae5687e372531a3d3d
btendu <goyalbhartendu@gmail.com>
2024-07-07 02:53:29 +0530

    intial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,57 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import cuid from "cuid";
+import PropTypes from "prop-types";
+import { format } from "date-fns";
+import { styled } from "@mui/material/styles";
+import { Trash ,BookCheck } from "lucide-react";
+
+const LightTooltip = styled(({ className, ...props }) => (
+  <Tooltip
+    {...props}
+    classes={{ popper: className }}
+    className="cursor-pointer"
+    slotProps={{
+      popper: {
+        sx: {
+          [`&.${tooltipClasses.popper}[data-popper-placement*="bottom"] .${tooltipClasses.tooltip}`]:
+            {
+              marginTop: "0px",
+            },
+          [`&.${tooltipClasses.popper}[data-popper-placement*="top"] .${tooltipClasses.tooltip}`]:
+            {
+              marginBottom: "0px",
+            },
+          [`&.${tooltipClasses.popper}[data-popper-placement*="right"] .${tooltipClasses.tooltip}`]:
+            {
+              marginLeft: "30px",
+            },
+          [`&.${tooltipClasses.popper}[data-popper-placement*="left"] .${tooltipClasses.tooltip}`]:
+            {
+              marginRight: "0px",
+            },
+        },
+      },
+    }}
+  />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: theme.palette.common.white,
+    color: "rgba(0, 0, 0, 0.87)",
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}));
 
 export default function Home() {
   const [picked, setPicked] = useState({});
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [selectedTitle, setSelectedTitle] = useState();
+  const [commitLogs, setCommitLogs] = useState([]);
+  const [discardingCommit, setDiscardingLogs] = useState([]);
+  const [selectedLog, setSelectedLog] = useState([]);
   const [boards, setBoards] = useState([
     {
       cardName: "Stuff to Try (this is a list)",
@@ -32,6 +79,13 @@ export default function Home() {
 
   let rIndex;
 
+  const generateRandomColor = () => {
+    const randomRed = Math.floor(Math.random() * 256);
+    const randomGreen = Math.floor(Math.random() * 256);
+    const randomBlue = Math.floor(Math.random() * 256);
+    return `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`;
+  };
+
   const handleDropped = (recievedElements, index) => {
     //Make changes in the original
     boards.map((item) => {
@@ -45,6 +99,18 @@ export default function Home() {
         rIndex = 0;
       }
     });
+    const color = generateRandomColor();
+    const currentTime = new Date();
+    const modifiedTimestamp = format(currentTime, "HH:mm a");
+    const log = {
+      commitId: cuid(),
+      time: modifiedTimestamp,
+      fromCard: picked,
+      toCard: recievedElements.cardName,
+      title: selectedTitle,
+      color: color,
+    };
+    setCommitLogs((prev) => [...prev, log]);
     //Save the changes back in state
     setBoards([...boards]);
   };
@@ -58,6 +124,64 @@ export default function Home() {
     setPicked(element);
     setSelectedIndex(index);
     setSelectedTitle(ele);
+  };
+
+  const variants = {
+    open: {
+      height: "100%",
+      transition: {
+        type: "spring",
+        duration: 1,
+      },
+    },
+    closed: {
+      height: "1/6",
+      transition: {
+        type: "spring",
+        stiffness: 500,
+        damping: 30,
+      },
+    },
+  };
+
+  const deleteCommit = () => {
+    const updatedCards = commitLogs.filter(
+      (itemCommit, index) => !discardingCommit.includes(itemCommit.commitId)
+    );
+    setCommitLogs(updatedCards);
+    const revertedCommits = boards.map((insertItem, indexItem) => {
+      selectedLog.forEach((e, i) => {
+        if (e.fromCard === insertItem.cardName) {
+          insertItem.items.push({ title: e.title.title });
+        }
+        if (e.toCard === insertItem.cardName) {
+          const indexfound = insertItem.items.findIndex(
+            (item) => item.title === e.title.title
+          );
+          insertItem.items.splice(indexfound, 1);
+        }
+      });
+      return insertItem;
+    });
+    setDiscardingLogs([]);
+  };
+
+  const handleKeep = () => {
+    setDiscardingLogs([]);
+  };
+
+  const handleCommit = (element, item) => {
+    const selectedIndex = commitLogs.findIndex(
+      (ele) => ele.commitId === item.commitId
+    );
+    const selectedCommit = commitLogs.filter(
+      (ele, ind) => ind >= selectedIndex
+    );
+    setSelectedLog(selectedCommit);
+    const commitIdArray = commitLogs
+      .map((commits, ind) => (ind >= selectedIndex ? commits.commitId : null))
+      .filter((comm) => comm !== null);
+    setDiscardingLogs([...commitIdArray]);
   };
 
   return (
@@ -101,6 +225,157 @@ export default function Home() {
             </div>
           </>
         ))}
+        <motion.div
+          className="relative flex pb-6 items-center overflow-auto flex-col w-full h-fit bg-white  rounded-lg mx-auto"
+          animate="open"
+          variants={variants}
+        >
+          <div
+            className="font-sans font-sans font-medium mb-1 mt-2 pb-4"
+            style={{ backgroundColor: "#ffffff" }}
+          >
+            Board Logs
+          </div>
+          <div className="relative flex items-center overflow-auto flex-col w-full h-fit bg-white  rounded-lg">
+            {discardingCommit.length > 0 ? (
+              <motion.div
+                animate={{ y: 6 }}
+                className="flex  mb-4"
+                transition={{ type: "spring", duration: 1 }}
+              >
+                <motion.div
+                  onClick={() => handleKeep()}
+                  whileHover={{ scale: 1.1 }}
+                  className="rounded-lg text-xs flex cursor-pointer mr-10"
+                >
+                  <BookCheck color="#00a1ff" size={18} className="mr-2" />{" "}
+                  <span className="pt-0.5">Keep</span>{" "}
+                </motion.div>
+                <motion.div
+                  onClick={() => deleteCommit()}
+                  whileHover={{ scale: 1.1 }}
+                  className="text-xs flex cursor-pointer "
+                >
+                  <Trash color="#ef0b60" size={18} className="mr-2" />{" "}
+                  <span className="pt-0.5">Remove</span>{" "}
+                </motion.div>
+              </motion.div>
+            ) : (
+              <div className=" mb-4"></div>
+            )}
+            <div className="flex flex-cols">
+              <div className="relative flex items-center overflow-auto flex-col w-full h-fit bg-white  rounded-lg">
+                <div className="flex flex-cols">
+                  <div className="mt-4 force-overflow pb-10">
+                    {commitLogs
+                      ?.slice()
+                      .reverse()
+                      .map((item, index) => (
+                        <motion.div
+                          key={index}
+                          animate={{ y: 6 }}
+                          transition={{ type: "spring" }}
+                        >
+                          <LightTooltip
+                            title={
+                              <div>
+                                <Typography
+                                  className="font-sans text-sm"
+                                  style={{
+                                    backgroundColor: "#ffffff",
+                                    color: "black",
+                                  }}
+                                >
+                                  <span
+                                    className="text-sm font-sans font-semibold mr-2"
+                                    style={{ color: `${item.color}` }}
+                                  >
+                                    From Card:
+                                  </span>{" "}
+                                  {item.fromCard}{" "}
+                                </Typography>
+                                <Typography
+                                  className="font-sans font-sans text-sm"
+                                  style={{ backgroundColor: "#ffffff" }}
+                                >
+                                  <span
+                                    className="text-sm font-semibold  mr-2 font-sans"
+                                    style={{ color: `${item.color}` }}
+                                  >
+                                    To Card:
+                                  </span>{" "}
+                                  {item.toCard}
+                                </Typography>
+                                <Typography
+                                  className="font-sans font-sans text-sm"
+                                  style={{ backgroundColor: "#ffffff" }}
+                                >
+                                  <span
+                                    style={{ color: `${item.color}` }}
+                                    className=" font-sans font-semibold mr-2 text-sm"
+                                  >
+                                    Title:
+                                  </span>{" "}
+                                  {item.title.title}
+                                </Typography>
+                              </div>
+                            }
+                            className="cursor-pointer"
+                            placement="right"
+                            arrow
+                          >
+                            <motion.div
+                              whileTap={{ scale: 0.8 }}
+                              whileHover={{ scale: 1.2 }}
+                              onHoverStart={(e) => {}}
+                              style={
+                                discardingCommit.includes(item.commitId)
+                                  ? { backgroundColor: "#ef0b60" }
+                                  : { backgroundColor: item.color }
+                              }
+                              onHoverEnd={(e) => {}}
+                              onClick={() => handleCommit(index, item)}
+                              key={index}
+                              className={`z-10 w-4 h-4 rounded-full `}
+                            ></motion.div>
+                          </LightTooltip>
+                          <motion.div
+                            style={
+                              discardingCommit.includes(item.commitId)
+                                ? { borderColor: "#ef0b60" }
+                                : {}
+                            }
+                            transition={{ type: "spring", duration: 5 }}
+                            className="z-0 ml-1.5 border w-0 h-10 border-slate-300"
+                          ></motion.div>
+                        </motion.div>
+                      ))}
+                  </div>
+                  <div className="mt-4">
+                    {commitLogs
+                      ?.slice()
+                      .reverse()
+                      ?.map((item, index) => (
+                        <motion.div
+                          animate={{ y: 6 }}
+                          transition={{ type: "spring" }}
+                          style={
+                            discardingCommit.includes(item.commitId)
+                              ? { color: "#ef0b60" }
+                              : {}
+                          }
+                          key={index}
+                          className="text-xs ml-7 w-fit h-14 "
+                        >
+                          {item.time}
+                        </motion.div>
+                      ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
